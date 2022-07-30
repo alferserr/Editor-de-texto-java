7ded29f494933d434fb6cec2dd97fc5360936019
alferserr <alferserr@hotmail.com>
2022-07-30 15:48:23 +0200

    Updated 30 de julio 15:48

--- src/fernandez/alejandro/editor/Main.java
+++ src/fernandez/alejandro/editor/Main.java
@@ -20,13 +20,74 @@ public class Main {
 
     static class Panel extends JPanel{
         public Panel(){
+            //------------------ Menu -----------------------------
+            JPanel panelMenu = new JPanel();
+            menu = new JMenuBar();
+            archivo = new JMenu("Archivo");
+            editar = new JMenu("Editar");
+            selección = new JMenu("Selección");
+            ver = new JMenu("Ver");
+            apariencia = new JMenu("Apariencia");
+
+            menu.add(archivo);
+            menu.add(editar);
+            menu.add(selección);
+            menu.add(ver);
+
+
+            //---------------------------- Elementos del menu archivo ----------------------------
+            creaItem("Nuevo archivo","archivo","nuevo");
+            creaItem("Abrir archivo","archivo", "abrir");
+            creaItem("Guardar","archivo","guardar");
+            creaItem("Guardar Como","archivo","guardarComo");
+            //------------------------------------------------------------------------------------------
+
+            //----------------------------- Elementos del menu editar -----------------------------
+            creaItem("Deshacer","editar","");
+            creaItem("Rehacer","editar","");
+            creaItem("Cortar","editar","");
+            creaItem("Copiar","editar","");
+            creaItem("Pegar","editar","");
+            //------------------------------------------------------------------------------------------
+
+            //----------------------------- Elementos del menu selección --------------------------
+            creaItem("Seleccionar todo","selección","");
+            //-------------------------------------------------------------------------------------------
+
+            //----------------------------- Elementos del menu ver ---------------------------------
+            creaItem("Numeración","ver","");
+            ver.add(apariencia);
+            creaItem("Normal","apariencia","");
+            creaItem("Dark","apariencia","");
+            //--------------------------------------------------------------------------------------------
+
+            panelMenu.add(menu);
+
 
             //------------------ Area de texto ---------------------
             tPane = new JTabbedPane();
             //--------------------------------------------------------
 
             creaPanel();
+            add(panelMenu);
             add(tPane);
+
+        }
+
+        public void creaItem(String rotulo, String menu, String accion){
+            elementoItem = new JMenuItem(rotulo);
+
+            if (menu.equals("archivo")){
+                archivo.add(elementoItem);
+            } else if (menu.equals("editar")) {
+                editar.add(elementoItem);
+            } else if (menu.equals("selección")) {
+                selección.add(elementoItem);
+            }else if (menu.equals("ver")) {
+                ver.add(elementoItem);
+            }else if (menu.equals("apariencia")) {
+                apariencia.add(elementoItem);
+            }
 
         }
 
@@ -42,5 +103,8 @@ public class Main {
         private JTabbedPane tPane;
         private JPanel ventana;
         private JTextPane areaTexto;
+        private JMenuBar menu;
+        private JMenu archivo, editar, selección, ver, apariencia;
+        private JMenuItem elementoItem;
     }
 }
